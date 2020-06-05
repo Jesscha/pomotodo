95b48f1169d76470cc7ec346c1be09fef25c202c
jessecha <jessecha222@gmail.com>
2020-06-05 16:03:47 +0900

    adding action on components

--- src/component/input-container/Inputcontainer.component.jsx
+++ src/component/input-container/Inputcontainer.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux';
+import { addItem } from '../../redux/todo/todo.action';
 
 
 class InputContainer extends React.Component{
@@ -24,20 +25,33 @@ class InputContainer extends React.Component{
         )
     }
 
-    render(){
+    itemAppending(callback){
+        // 중복되지 않은 아이디를 만들도록 년도+월+시+분+초를 더해서 아이디로 씀
+        const date = new Date()
+        const id = date.getFullYear() + date.getMonth() + date.getDate() + date.getHours() + date.getMinutes() + date.getSeconds()
+        
+        const item = {
+            id,
+            todoInput: this.state.todoInput,
+            pomoCount: this.state.pomoCount
+        }
+        callback(item)
+        this.initializeState();
+    }
+
+    render({addItem}){
         return(
             <div className="input-container">
                 <input type="text" name="todoInput" value={this.state.todoInput} id="todo-input" onChange={(e)=>this.changeState(e)} placeholder="To-Do Item"/>
                 <input type="number" name="pomoCount" onChange={(e)=>this.changeState(e)} value={this.state.pomoCount} id="pomo-count"/>
-                <button onClick={()=>{this.initializeState()}}>Add to List</button>
+                <button onClick={()=>{this.itemAppending(addItem)}}>Add to List</button>
             </div>
         )
     }
-   
 }
 
 const mapDispatchToProps = dispatch =>{
-    
+    addItem: item => dispatch(addItem(item))
 }
 
-export default InputContainer
+export default connect(null, mapDispatchToProps)(InputContainer);
