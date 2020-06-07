7f0ced6e73495c3bb7395a93e2ac91c9ef3b3d1a
jessecha <jessecha222@gmail.com>
2020-06-07 22:00:46 +0900

    add persistor

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,19 @@
 import { combineReducers } from 'redux';
 import todoReducer from './todo/todo.reducer';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+
+const persistConfig = {
+    key : 'root',
+    storage,
+    whitelist: ['todoItem']
+};
 
 const rootReducer = combineReducers({
     todoItem : todoReducer
    
-})
+});
 
 
 
-export default rootReducer;
+export default persistReducer(persistConfig, rootReducer);
