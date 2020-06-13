1e6898e88a473e5ff4ef7110ec03793db45c5fda
jessecha <jessecha222@gmail.com>
2020-06-13 23:09:38 +0900

    change default timing

--- src/assets/todo.variables.js
+++ src/assets/todo.variables.js
@@ -1,5 +1,5 @@
 export const maxPomoBlocks = 7;
 export const itemPerTodoList = 5;
 export const itemPerDoneList = 4;
-export const workTime = 5; //초
-export const restingTime = 10; // 초
+export const workTime = 1500; //초
+export const restingTime = 300; // 초
