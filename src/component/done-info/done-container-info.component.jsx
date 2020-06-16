9ee6184b1fb9130758037c592e8cbcb35e5337c8
jessecha <jessecha222@gmail.com>
2020-06-16 17:01:34 +0900

    spacing

--- src/component/done-info/done-container-info.component.jsx
+++ src/component/done-info/done-container-info.component.jsx
@@ -7,13 +7,13 @@ import './done-container-info.styles.scss'
 
 export const DoneContainerInfo = () => {
     return (
-                <ul className="done-info">
-                    <li>
-                        <p className="description">- Click <Checkbox color="secondary" checked={true} /> to revive the task.</p>
-                    </li>
-                    <li>
-                        <p className="description">- <ButtonGroup color="secondary" className="button-description"><Button>X</Button></ButtonGroup> deletes the task from memory. It can't be reverted.</p>
-                    </li>
-                </ul>
+            <ul className="done-info">
+                <li>
+                    <p className="description">- Click <Checkbox color="secondary" checked={true} /> to revive the task.</p>
+                </li>
+                <li>
+                    <p className="description">- <ButtonGroup color="secondary" className="button-description"><Button>X</Button></ButtonGroup> deletes the task from memory. It can't be reverted.</p>
+                </li>
+            </ul>
     )
 }
