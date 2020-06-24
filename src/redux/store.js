4e6c6df5fac9f577cfd751ebbd34dd8b5474e024
jessecha <jessecha222@gmail.com>
2020-06-24 14:51:57 +0900

    change config

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,17 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from 'redux';
 
-import logger from "redux-logger";
-import rootReducer from "./root-reducer";
-import { persistStore } from "redux-persist";
+import logger from 'redux-logger';
+import rootReducer from './root-reducer';
+import { persistStore } from 'redux-persist';
 
+const middlewares = [];
 
-const middlewares = [logger];
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
+
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
 
-
-
-export default {store, persistor}
+export default { store, persistor };
